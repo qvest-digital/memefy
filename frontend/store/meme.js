2726588b63a0a81da8344b4343939cd39cc24259
sschum <s.schumann@tarent.de>
2018-09-21 11:28:35 +0200

    initial vuex store

---
+++ frontend/store/meme.js
@@ -0,0 +1,45 @@
+
+export const state = () => ({
+    memes: []
+})
+
+export const mutations = {
+    addMeme(state, meme) {
+        state.memes.push(meme)
+    }
+}
+
+const actions = {
+    init(vuexContext) {
+        return new Promise((resolve, reject) => {
+            //TODO: ask for memes
+
+            console.log("HAllo ?")
+
+            vuexContext.commit('addMeme', {
+                id: 'id1',
+                name: 'Testname',
+                pic: 'https://media.giphy.com/media/gSIz6gGLhguOY/giphy.gif',
+                sound: 'http://...'
+            })
+
+            setTimeout(() => {
+                vuexContext.commit('addMeme', {
+                    id: 'id2',
+                    name: 'Testname2',
+                    pic: 'https://media.giphy.com/media/xUPGcA1SkYqVLDtxiU/giphy.gif',
+                    sound: 'http://...'
+                })
+            }, 5000)
+
+            resolve()
+        });
+    },
+}
+
+export default {
+    namespaced: true,
+    state,
+    mutations,
+    actions
+}
