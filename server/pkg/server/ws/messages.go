eb8ca86d979feb5ccefd4ebebff749b5c241a60e
Dino Omanovic <d.omanovic@tarent.de>
2018-09-21 14:48:03 +0200

    fix json naming

--- server/pkg/server/ws/messages.go
+++ server/pkg/server/ws/messages.go
@@ -1,9 +1,9 @@
 package ws
 
 type ClientRegistration struct {
-	currentMemes []string `json:"currentMemes"`
+	CurrentMemes []string `json:"currentMemes"`
 }
 
 type NewMemes struct {
-	newMemes []string `json:"newMemes"`
+	NewMemes []string `json:"newMemes"`
 }
