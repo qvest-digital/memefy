1bdd8dd9b796270fbebcdb95cd98e102a4854940
sschum <s.schumann@tarent.de>
2018-09-21 15:49:58 +0200

    implements upload validation

--- frontend/plugins/global_components.js
+++ frontend/plugins/global_components.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
+import Vuelidate from 'vuelidate'
 
 import Success from '../components/message/Success'
 import Warning from '../components/message/Warning'
 import Info from '../components/message/Info'
 import Error from '../components/message/Error'
 
+Vue.use(Vuelidate)
 Vue.component('message-success', Success)
 Vue.component('message-warning', Warning)
 Vue.component('message-info', Info)
