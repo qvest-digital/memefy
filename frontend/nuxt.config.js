589f2d123454f3b4f860c9129a52c019cd3097ac
sschum <s.schumann@tarent.de>
2018-09-21 15:25:04 +0200

    fix the skin settings

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -16,7 +16,10 @@ module.exports = {
         ],
         link: [
             {rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'}
-        ]
+        ],
+        bodyAttrs: {
+            class: 'skin-black sidebar-mini'
+        }
     },
 
     /*
